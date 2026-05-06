43af4695ae2458922702a1d7b5caf3a8ffa032a8
nvjyotsngh <nvjyotsngh@gmail.com>
2026-05-06 22:39:14 +0530

    cs50p week 1 problems

---
+++ deep thought.py
@@ -0,0 +1,8 @@
+user_input = input("What is the Great Question of Life, the Universe and Everything??")
+
+right_answer = "42", "fotty-two", "fotty two"
+
+if user_input in right_answer:
+    print("yes")
+else:
+    print("no")
