19fb5e3ad3014fca4f7a442228ba1e23417ad97a
nvjyotsngh <nvjyotsngh@gmail.com>
2026-05-16 04:15:16 +0530

    cs50p week 1 math interpreter

---
+++ interpreter.py
@@ -0,0 +1,13 @@
+x, y, z = input("Expression: ").split()
+
+x = float(x)
+z = float(z)
+
+if y == "+":
+    print(f"{x + z:.1f}")
+elif y == "-":
+    print(f"{x - z:.1f}")
+elif y == "*":
+    print(f"{x * z:.1f}")
+else:
+    print(f"{x / z:.1f}")
