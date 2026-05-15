e8160c812e36a21dd6fec31d024110931a600bc6
nvjyotsngh <nvjyotsngh@gmail.com>
2026-05-16 03:41:39 +0530

    cs50p week 1 banking problem

--- deep thought.py
+++ deep thought.py
@@ -1,6 +1,6 @@
-user_input = input("What is the Great Question of Life, the Universe and Everything??")
+user_input = input("What is the Great Question of Life, the Universe and Everything??").lower()
 
-right_answer = "42", "fotty-two", "fotty two"
+right_answer = "42", "forty-two", "forty two"
 
 if user_input in right_answer:
     print("yes")
